f87e11e4f7456d26899af04e99a68b45b67e41f2
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-09 17:41:35 +0300

    performed CRUD operations using SQL

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -13,23 +13,29 @@ select_all_bears_names_and_orders_in_alphabetical_order = """
     ORDER BY name
 """
 
-# OKAY TILL THIS PART
-
-alive = True
-dead = 0
-
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = f"""
+select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
     SELECT
         bears.name,
         bears.age
     FROM bears
+    WHERE alive = 1
     ORDER BY age   
-    WHERE alive = {alive};
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY age DESC
+    LIMIT 1
 """
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+    SELECT
+        bears.name,
+        bears.age
+    FROM bears
+    ORDER BY age ASC
+    LIMIT 1
 """
+
